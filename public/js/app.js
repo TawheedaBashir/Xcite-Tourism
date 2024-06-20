21f5f1c444d3109230486f5abd6aa7efb730e4a5
TAWSEEF SHAMIM <tawseefshamim@gmail.com>
2024-06-20 13:26:33 +0530

    contact form tested

--- public/js/app.js
+++ public/js/app.js
@@ -1,33 +1,72 @@
-const contactForm = document.querySelector('.contact-form');
-let name = document.getElementById('Name');
-let email = document.getElementById('email');
-let message = document.getElementById('message');
-contactForm.addEventListener('submit',(e) =>{
-   e.preventDefault();
-let formData = {
-    name: Name.value,
-    email: email.value,
-    message: message.value
+// const contactForm = document.querySelector('.contact-form');
+// let name = document.getElementById('Name');
+// let email = document.getElementById('email');
+// let message = document.getElementById('message');
+// contactForm.addEventListener('submit',(e) =>{
+//    e.preventDefault();
+// let formData = {
+//     name: Name.value,
+//     email: email.value,
+//     message: message.value
 
-}
-let xhr = new XMLHttpRequest();
-  xhr.open ('POST','/');
-  xhr.setRequestHeader('content-type', 'application/json');
-  xhr.onload = function()
-  {
-console.log(xhr.responseText);
-if (xhr.responseText == 'success'){
-    alert('Thank you for contacting us!');
-    Name.value = '';
-    email.value = '';
-    message.value = '';
+// }
+// let xhr = new XMLHttpRequest();
+//   xhr.open ('POST','/');
+//   xhr.setRequestHeader('content-type', 'application/json');
+//   xhr.onload = function()
+//   {
+// console.log(xhr.responseText);
+// if (xhr.responseText == 'success'){
+//     alert('Thank you for contacting us!');
+//     Name.value = '';
+//     email.value = '';
+//     message.value = '';
 
-} else
-{
-    alert('something went wrong!')
-}
-  }
-  xhr.send(JSON.stringify(formData));
+// } else
+// {
+//     alert('something went wrong!')
+// }
+//   }
+//   xhr.send(JSON.stringify(formData));
 
 
-})
+// })
+
+const form = document.getElementById('form');
+
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+              alert('Thank you for contacting us!');
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
+});
--- server.js
+++ server.js
@@ -1,57 +1,46 @@
-const  express = require('express');
+require('dotenv').config();
+const express = require('express');
 const app = express();
 const nodemailer = require("nodemailer");
 const PORT = process.env.PORT || 5001;
-//Middleware
 
+// Middleware
 app.use(express.static('public'));
-app.use(express.json())
+app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/contactform.html');
+});
 
+app.post('/', (req, res) => {
+    console.log('Request Body:', req.body);
 
-app.get('/',(req , res)=>{
-    res.sendFile(__dirname +'/public/contactform.html')
- })
-app.post('/', (req , res)=>{
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.USERNAME,
+            pass: process.env.PASSWORD,
+        }
+    });
 
-    console.log(req.body);
-const transporter = nodemailer.createTransport({
+    const mailOptions = {
+        from: req.body.email,
+        to: process.env.USERNAME,
+        subject: `Message from ${req.body.email}: ${req.body.name}`,
+        text: req.body.message
+    };
 
-  service:'gmail',
-  auth:{
-  user:'xcitetourismtourandtravels@gmail.com',
-  pass:'pglb avsc xfbz pdzk'
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error occurred:', error);
+            return res.status(500).send('Error sending email');
+        } else {
+            console.log('Email sent: ' + info.response);
+            return res.status(200).send('Email sent successfully');
+        }
+    });
+});
 
-
-  }
-})
-const  mailOptions = {
-    from: req.body.email,
-    to:'xcitetourismtourandtravels@gmail.com',
-    subject:`Message from ${req.body.email}: ${req.body.name}`,
-    text: req.body.message
-
-}
-transporter.sendMail(mailOptions , (error, info)=>{
-  if(error){
-
-    console.log(error);
-    res.send(error);
-  }
-  else{
-    console.log('Email sent:' +info.response);
-    res.send('success')
-  }
-})
-})
-
-  app.listen(PORT, ()=>{
-
-   console.log(`Server running on port ${PORT}`);
-
-  })
-
-
-
-
-
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
